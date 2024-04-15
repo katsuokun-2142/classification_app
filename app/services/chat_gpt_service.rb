908903e49ea821be0afcf18ae42a08b2a654a9ce
katsuokun-2142 <b2a1t4t2lefield@gmail.com>
2024-04-15 18:17:40 +0900

    chatGPT_APIクイック設定

---
+++ app/services/chat_gpt_service.rb
@@ -0,0 +1,19 @@
+class ChatGptService
+  require 'openai'
+
+  def initialize
+    @openai = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_ACCESS_TOKEN"))
+  end
+
+  def chat(prompt)
+    response = @openai.chat(
+      parameters: {
+        model: "gpt-3.5-turbo", # Required. # 使用するGPT-3のエンジンを指定
+        messages: [{ role: "system", content: "You are a helpful assistant. response to japanese" }, { role: "user", content: prompt }],
+        temperature: 0.7, # 応答のランダム性を指定
+        max_tokens: 200,  # 応答の長さを指定
+      },
+      )
+    response['choices'].first['message']['content']
+  end
+end
